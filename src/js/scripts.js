2357afe5b6e27db89a0eed910ab53634a8c9af0d
Yegor Zaporozhets <egorserg4@gmail.com>
2018-12-19 12:41:54 +0300

    Add contact block and sctroll to doc points

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,35 @@
-// Your code here...
+
+const header = document.querySelector('header');
+const contactsKey = document.querySelector('#openContacts');
+const contactsDiv = document.querySelector('#contactsBlock');
+
+contactsKey.addEventListener('click', () => {
+    const styles = getComputedStyle(contactsDiv);
+
+    if (parseInt(styles.marginTop, 10)) {
+        contactsDiv.style.marginTop = 0;
+    } else {
+        contactsDiv.style.marginTop = '-100px';
+    }
+});
+
+header.addEventListener('click', event => {
+    event.preventDefault();
+
+    const target = event.target;
+
+    if (!target.dataset.goTo) {
+        return;
+    }
+
+    // if (target.tagName !== 'A') {
+    //     return;
+    // }
+
+    const goTo = target.dataset.goTo;
+    console.log('.' + goTo);
+    const elementToScroll = document.querySelector(`.${goTo}`);
+    const top = elementToScroll.getBoundingClientRect().top;
+
+    window.scrollTo({top, behavior: 'smooth'});
+});
